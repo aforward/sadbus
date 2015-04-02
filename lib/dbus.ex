d2da08e2ef6f1bcb96edb13e0dd7303d940cae29
Andrew Forward <aforward@gmail.com>
2015-04-02 09:47:01 -0400

    Added a simple process

--- lib/dbus.ex
+++ lib/dbus.ex
@@ -19,9 +19,43 @@ defmodule Dbus do
     Supervisor.start_link(children, opts)
   end
 
-  def kill(), do: R.q(["DEL", "topics"])
+  def kill()do
+    R.q!(["SMEMBERS", "topics"]) |> Enum.map(&unregister(&1))
+    R.q(["DEL", "topics"])
+  end
   def topics(), do: R.q!(["SMEMBERS","topics"])
-  def add_topic(name), do: R.q!(["SADD","topics",name])
+  def register(name), do: R.q!(["SADD","topics",name])
+  def unregister(name) do
+    R.q!(["SREM","topics",name])
+    R.q!(["DEL","topics.#{name}"])
+  end
+  def pub(topic,msg), do: R.q!(["RPUSH", "topics.#{topic}", msg |> serialize])
+  def peek(topic), do: _peek(topic, 0)
+  def peek(_topic, 0), do: []
+  def peek(topic, :all), do: _peek(topic, 0)
+  def peek(topic,num), do: _peek(topic, num)
+  def pop(topic), do: pop(topic, :next)
+  def pop(topic, :next), do: R.q!(["LPOP","topics.#{topic}"]) |> deserialize
+  def pop(_topic, 0), do: []
+  def pop(topic, -1), do: pop(topic, :all)
+  def pop(topic, :all) do
+    answer = peek(topic, :all)
+    R.q!(["DEL","topics.#{topic}"])
+    answer
+  end
+  def pop(topic, num), do: 1..num |> Enum.map(fn(_i) -> pop(topic, :next) end) |> Enum.filter(&(!is_nil(&1)))
+  def size(topic), do: R.q!(["LLEN","topics.#{topic}"]) |> String.to_integer
+  def process(topic, my_fn), do: process(topic, my_fn, :all)
+  def process(topic, my_fn, num) do
+    pop(topic, num)
+    |> Enum.map(&(my_fn.(&1)))
+  end
 
+
+  defp serialize(msg), do: :erlang.term_to_binary(msg)
+  defp deserialize(:undefined), do: nil
+  defp deserialize(msg), do: :erlang.binary_to_term(msg)
+  defp deserialize_all(msgs), do: Enum.map(msgs, &deserialize/1)
+  defp _peek(topic, num), do: R.q!(["LRANGE","topics.#{topic}",0,num - 1]) |> deserialize_all
 
 end
--- test/dbus_test.exs
+++ test/dbus_test.exs
@@ -1,8 +1,13 @@
 defmodule DbusTest do
   use ExUnit.Case
 
+  def xxx_fn(args) do
+    Dbus.Redis.q(["RPUSH","test",args[:var]])
+  end
+
   setup do
     Dbus.kill
+    Dbus.Redis.q(["del","test"])
     { :ok, [] }
   end
 
@@ -11,9 +16,99 @@ defmodule DbusTest do
   end
 
   test "topics - add some" do
-    Dbus.add_topic("xx")
-    Dbus.add_topic("yy")
+    Dbus.register("xx")
+    Dbus.register("yy")
     assert (Dbus.topics() |> Enum.sort) == ["xx", "yy"]
   end
+
+  test "pub" do
+    Dbus.register("xxx")
+    Dbus.pub("xxx", [var: "1"])
+    assert Dbus.peek("xxx") == [[var: "1"]]
+    Dbus.pub("xxx", [var: "2"])
+    assert Dbus.peek("xxx") == [[var: "1"], [var: "2"]]
+  end
+
+  test "pop" do
+    Dbus.register("xxx")
+    Dbus.pub("xxx", [var: "1"])
+    Dbus.pub("xxx", [var: "2"])
+    assert Dbus.pop("xxx") == [var: "1"]
+    assert Dbus.peek("xxx") == [[var: "2"]]
+  end
+
+  test "pop - next" do
+    Dbus.register("xxx")
+    Dbus.pub("xxx", [var: "1"])
+    Dbus.pub("xxx", [var: "2"])
+    assert Dbus.pop("xxx", :next) == [var: "1"]
+    assert Dbus.peek("xxx") == [[var: "2"]]
+  end
+
+  test "pop - all" do
+    Dbus.register("xxx")
+    Dbus.pub("xxx", [var: "1"])
+    Dbus.pub("xxx", [var: "2"])
+    Dbus.pub("xxx", [var: "3"])
+    Dbus.pub("xxx", [var: "4"])
+    Dbus.pub("xxx", [var: "5"])
+    assert Dbus.size("xxx") == 5
+    assert Dbus.pop("xxx", 0) == []
+    assert Dbus.size("xxx") == 5
+    assert Dbus.pop("xxx", 2) == [[var: "1"], [var: "2"]]
+    assert Dbus.size("xxx") == 3
+    assert Dbus.pop("xxx", :all) == [[var: "3"], [var: "4"], [var: "5"]]
+
+    Dbus.pub("xxx", [var: "a"])
+    Dbus.pub("xxx", [var: "b"])
+    assert Dbus.pop("xxx", -1) == [[var: "a"], [var: "b"]]
+    assert Dbus.size("xxx") == 0
+
+    Dbus.pub("xxx", [var: "x"])
+    assert Dbus.pop("xxx", 99) == [[var: "x"]]
+  end
+
+  test "peek - all" do
+    Dbus.register("xxx")
+    Dbus.pub("xxx", [var: "1"])
+    Dbus.pub("xxx", [var: "2"])
+    Dbus.pub("xxx", [var: "3"])
+    assert Dbus.peek("xxx") == [[var: "1"], [var: "2"], [var: "3"]]
+    assert Dbus.peek("xxx", :all) == [[var: "1"], [var: "2"], [var: "3"]]
+    assert Dbus.peek("xxx",0) == []
+    assert Dbus.peek("xxx",99) == [[var: "1"], [var: "2"], [var: "3"]]
+    assert Dbus.peek("xxx",2) == [[var: "1"], [var: "2"]]
+  end
+
+  test "size" do
+    Dbus.register("xxx")
+    assert Dbus.size("xxx") == 0
+    Dbus.pub("xxx", [var: "1"])
+    Dbus.pub("xxx", [var: "2"])
+    assert Dbus.size("xxx") == 2
+    Dbus.pop("xxx")
+    assert Dbus.size("xxx") == 1
+  end
+
+  test "process - all" do
+    Dbus.register("xxx")
+    Dbus.pub("xxx", [var: "one"])
+    Dbus.pub("xxx", [var: "deux"])
+    assert Dbus.size("xxx") == 2
+    Dbus.process("xxx",&xxx_fn/1)
+    assert Dbus.size("xxx") == 0
+    assert Dbus.Redis.q!(["LRANGE", "test", 0, -1]) == ["one","deux"]
+  end
+
+  test "process - one" do
+    Dbus.register("xxx")
+    Dbus.pub("xxx", [var: "one"])
+    Dbus.pub("xxx", [var: "deux"])
+    assert Dbus.size("xxx") == 2
+    Dbus.process("xxx", &xxx_fn/1, 1)
+    assert Dbus.size("xxx") == 1
+    assert Dbus.Redis.q!(["LRANGE", "test", 0, -1]) == ["one"]
+  end
+
 
 end
